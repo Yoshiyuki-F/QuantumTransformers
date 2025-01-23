f37692058e467abcea23c119d65834e51db5e334
fukuda <y.fukuda@campus.lmu.de>
2025-01-23 16:07:04 +0100

    cpu 10 temp

--- src/hpopt/hpopt.py
+++ src/hpopt/hpopt.py
@@ -146,7 +146,7 @@ if __name__ == '__main__':
 
     ray.init()
 
-    resources_per_trial = {"cpu": 224, "gpu": 0}
+    resources_per_trial =  {"cpu": 10, "gpu": 0}#{"cpu": 224, "gpu": 0}
     tuner = tune.Tuner(
         tune.with_resources(train, resources=resources_per_trial),
         tune_config=tune.TuneConfig(
